5f9e5dbc736c76e9c625cc3fee16eea5c3a53380
dani <danifromecuador@gmail.com>
2025-02-23 16:28:05 -0500

    chore: shrink code using ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import bgImg1 from './assets/backgrounds/-red-sunset-wallpaper.jpg'
 import bgImg2 from './assets/backgrounds/0001-pixels.jpg'
 
+// TODO: include a black background image
+
 export const App = () => {
   const imagesArray = [bgImg1, bgImg2]
   const [index, setIndex] = useState(0)
-  const changeBackground = () => {
-    if (index < imagesArray.length - 1) setIndex((index) => index + 1)
-    else setIndex(0)
-  }
+  const changeBackground = () =>
+    index >= imagesArray.length - 1 ? setIndex(0) : setIndex(index + 1)
 
   return (
     <div className='app'>
