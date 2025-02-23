69957d6243684b513f435fc6cbb35c6c3d45198c
dani <danifromecuador@gmail.com>
2025-02-23 15:38:26 -0500

    chore: set a background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import './App.css'
+import backgroundImage from './assets/backgrounds/-red-sunset-wallpaper.jpg'
+// import backgroundImage1 from './assets/backgrounds/0001-pixels.jpg';
 
 export const App = () => {
-  return <div className='app'>Hello Dani</div>
+  // const bgImgUrl = ['./assets/backgrounds/-red-sunset-wallpaper.jpg', './assets/backgrounds/0001-pixels.jpg']
+  return (
+    <div className='app'>
+      Hello Dani
+      <button type='button'>change background</button>
+      <img
+        src={backgroundImage}
+        alt='background-image'
+        className='background-image'
+      />
+    </div>
+  )
 }
