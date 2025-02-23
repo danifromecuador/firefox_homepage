07461647abe021c9602ca0ea71d6cc6e57f4405f
dani <danifromecuador@gmail.com>
2025-02-23 15:54:14 -0500

    feat: change background when clicking on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import './App.css'
-import backgroundImage from './assets/backgrounds/-red-sunset-wallpaper.jpg'
-// import backgroundImage1 from './assets/backgrounds/0001-pixels.jpg';
+import bgImg1 from './assets/backgrounds/-red-sunset-wallpaper.jpg'
+import bgImg2 from './assets/backgrounds/0001-pixels.jpg'
 
 export const App = () => {
-  // const bgImgUrl = ['./assets/backgrounds/-red-sunset-wallpaper.jpg', './assets/backgrounds/0001-pixels.jpg']
+  const imagesArray = [bgImg1, bgImg2]
+  const [index, setIndex] = useState(0)
+  const changeBackground = () => {
+    setIndex((index) => index + 1)
+  }
+
   return (
     <div className='app'>
       Hello Dani
-      <button type='button'>change background</button>
+      <button type='button' onClick={() => changeBackground()}>
+        change background
+      </button>
       <img
-        src={backgroundImage}
+        src={imagesArray[index]}
         alt='background-image'
         className='background-image'
       />
